cfdca4b8f045c433c19c050cf6d00e2351cfcb3c
ahlaaa <36953985+ahlaaa@users.noreply.github.com>
2023-10-18 09:54:52 +0800

    Update index.js

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,5 +3,18 @@ import bootstrap from "bootstrap/dist/js/bootstrap.min.js";
 export const useAppStore = defineStore("storeId", {
   state: () => ({
     bootstrap,
+    isLoggedIn: false,
+    user: {},
   }),
+
+  actions: {
+    login(user) {
+      this.isLoggedIn = true;
+      this.user = user;
+    },
+    logout() {
+      this.isLoggedIn = false;
+      this.user = null;
+    },
+  },
 });
